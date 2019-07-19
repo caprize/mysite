79faf595c6be0df4be3d0cd4817f5d73a9afdfba
caprize <iskan20022@mail.ru>
2019-07-19 15:46:01 +0300

    Changed the HTML for the site.

--- mysite/settings.py
+++ mysite/settings.py
@@ -11,7 +11,6 @@ https://docs.djangoproject.com/en/2.0/ref/settings/
 """
 
 import os
-tokeennn=7959df124cc54d138c6f11a3dd6dd162d45e49f4
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 
