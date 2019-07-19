35df41052b80ae6751a6b204f5147c44aa2e80f9
caprize <iskan20022@mail.ru>
2019-07-19 15:25:08 +0300

    dd

--- mysite/settings.py
+++ mysite/settings.py
@@ -11,7 +11,7 @@ https://docs.djangoproject.com/en/2.0/ref/settings/
 """
 
 import os
-
+tokeennn=7959df124cc54d138c6f11a3dd6dd162d45e49f4
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 
@@ -41,7 +41,6 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'blog',
-    'telebot',
     
 ]
 
