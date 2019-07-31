4da7ebf98813fa75729ecb1e1b17aeda892f208d
caprize <iskan20022@mail.ru>
2019-07-31 22:23:38 +0300

    dada

--- blog/urls.py
+++ blog/urls.py
@@ -12,4 +12,4 @@ urlpatterns = [
     path('post/send_bot/', views.send_bot, name='send_bot'),
     path('accounts/register/', views.Register, name="register"),
 
-]  + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)+static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
+]  + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
--- mysite/settings.py
+++ mysite/settings.py
@@ -80,14 +80,16 @@ WSGI_APPLICATION = 'mysite.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': 'djangotest',
+        'USER': 'iskander',
+        'PASSWORD': 'vbhvbh123',
+        'HOST': 'localhost',
+        'PORT': '',
     }
 }
 
 
-# Password validation
-# https://docs.djangoproject.com/en/2.0/ref/settings/#auth-password-validators
 
 AUTH_PASSWORD_VALIDATORS = [
     {
